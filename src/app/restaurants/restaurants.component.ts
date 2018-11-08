8f09d9bd821b0aaa3e012ae755a0b70f71df5b9a
Herrick Wolber <wolbereric@yahoo.fr>
2018-11-08 19:25:31 +0100

    Update src/app/restaurants/restaurants.component.ts

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,6 +4,7 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 
 import { RestaurantsService } from './restaurants.service';
 
+import { Plat } from './plat';
 
 @Component({
 	selector: 'app-restaurants',
