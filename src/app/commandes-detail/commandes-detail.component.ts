18b6b4d74eb234b42df73089d1b65ac8196fbe6c
Herrick Wolber <wolbereric@yahoo.fr>
2018-11-09 06:55:48 +0100

    Mise a jour détails

--- src/app/commandes-detail/commandes-detail.component.ts
+++ src/app/commandes-detail/commandes-detail.component.ts
@@ -4,7 +4,9 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Commandes } from '../commandes/commandes';
+import { Restaurant } from '../restaurants/restaurant';
 import { CommandesService } from '../commandes/commandes.service';
+import { RestaurantsService } from '../restaurants/restaurants.service';
 
 @Component({
 	selector: 'app-commandes-detail',
@@ -12,10 +14,12 @@ import { CommandesService } from '../commandes/commandes.service';
 	styleUrls: ['./commandes-detail.component.css']
 })
 export class CommandesDetailComponent implements OnInit {
-	commande: Commandes;
+	comm: Commandes;
+	restaurant: Restaurant;
 
 	constructor(
 		private commandeService: CommandesService, 
+		private restauService: RestaurantsService,
 		private route: ActivatedRoute,
   		private location: Location
   	) {}
@@ -24,9 +28,14 @@ export class CommandesDetailComponent implements OnInit {
 		this.getCommand();
 	}
 
+	getRestauInfos(){
+		this.restauService.getRestaurantById(this.comm.restaurant).subscribe(rest => this.restaurant = rest);
+	}
+
 	getCommand() {
 		const id = +this.route.snapshot.paramMap.get('id');
-		this.commandeService.getCommande(id).subscribe(comm => this.commande = comm);
+		this.commandeService.getCommande(id).subscribe(comm => this.comm = comm);
+		this.getRestauInfos();
 	}
 
 	goBack(): void {
